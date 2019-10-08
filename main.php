6e6eae928db211e2215c797d67758c580d5470fb
Nashwan Doaqan <nashwan.doaqan@gmail.com>
2019-10-09 00:08:10 +0300

    Reinstall Plugins v0.2

--- main.php
+++ main.php
@@ -1,19 +1,36 @@
 <?php
 /**
  * Plugin Name: Reinstall Plugins
- * Plugin URI: https://github.com/nash-ye/WP-Reinstall-Plugins
+ * Plugin URI: https://github.com/nash-ye/wp-reinstall-plugins
  * Description: A simple plugin to allow plugins re-installation.
  * Author: Nashwan Doaqan
- * Author URI: http://nashwan-d.com
- * Version: 0.1
+ * Author URI: https://profiles.wordpress.org/alex-ye/
+ * Version: 0.2
  *
  * License: GPL3+
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
- * Copyright (c) 2016 - 2017 Nashwan Doaqan.  All rights reserved.
+ * Copyright (c) 2016 - 2019 Nashwan Doaqan.  All rights reserved.
  */
 
-// Load the Reinstall_Plugins class.
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-reinstall-plugins.php';
+namespace ReinstallPlugins;
 
-// Instance the Reinstall_Plugins class.
-add_action( 'plugins_loaded', array( 'Reinstall_Plugins', 'instance' ), 99 );
+add_filter('upgrader_package_options', 'ReinstallPlugins\filterUpgraderPackageOptions');
+
+/**
+ * Filter upgrader options to allow plugin's re-installation.
+ *
+ * @param array $options Options used by the upgrader.
+ *
+ * @return array
+ * @since  0.2
+ */
+function filterUpgraderPackageOptions(array $options)
+{
+    if (isset($options['hook_extra']['type']) && isset($options['hook_extra']['action'])) {
+        if ('plugin' === $options['hook_extra']['type'] && 'install' === $options['hook_extra']['action']) {
+            $options['clear_destination'] = true;
+        }
+    }
+
+    return $options;
+}
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Reinstall Plugins ===
 Contributors: alex-ye
 Tags: plugin, plugins, upload
-Requires at least: 4.3.0
-Tested up to: 4.6.0
-Stable tag: 0.1
+Requires at least: 5.2.0
+Tested up to: 5.2.3
+Stable tag: 0.2
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
 
@@ -14,7 +14,7 @@ A simple plugin to allow plugins re-installation.
 Reinstall Plugins is a simple plugin to allow plugins re-installation, by using WordPress standard plugin update process.
 
 = Contributing =
-Developers can contribute to the source code on the [Github Repository](https://github.com/nash-ye/WP-Reinstall-Plugins).
+Developers can contribute to the source code on the [Github Repository](https://github.com/nash-ye/wp-reinstall-plugins).
 
 == Installation ==
 
@@ -22,6 +22,9 @@ Developers can contribute to the source code on the [Github Repository](https://
 2. Upload the plugin you want to re-install.
 
 == Changelog ==
+
+= 0.2 =
+* Update codebase.
 
 = 0.1 =
 * The Initial version.
